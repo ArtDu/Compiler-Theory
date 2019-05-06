11a3a672726f394301a7a3487bb3e60b33db23b3
ArtDub <rusartdub@gmail.com>
2019-05-06 14:22:51 +0300

    study

--- kp/curs1/code-gen.cpp
+++ kp/curs1/code-gen.cpp
@@ -1,4 +1,4 @@
-/* $mlisp19 */
+/* $a07 */
 #include "code-gen.h"
 using namespace std;
 int tCG::p01(){ // S -> PROG
@@ -8,215 +8,195 @@ int tCG::p01(){ // S -> PROG
   header += "//________________ \n";
   S1->obj = header + S1->obj;
 	return 0;}
-int tCG::p02(){ //    PROG -> CALCS
+int tCG::p02(){ //   PROG -> CALCS
 	return 0;}
-int tCG::p03(){ //    PROG -> DEFS
+int tCG::p03(){ //   PROG -> DEFS
 	return 0;}
-int tCG::p04(){ //    PROG -> DEFS CALCS
+int tCG::p04(){ //   PROG -> DEFS CALCS
 	return 0;}
-int tCG::p05(){ //   CALCS -> CALC
+int tCG::p05(){ //  CALCS -> CALC
 	return 0;}
-int tCG::p06(){ //   CALCS -> CALCS CALC
+int tCG::p06(){ //  CALCS -> CALCS CALC
 	return 0;}
-int tCG::p07(){ //    CALC -> E
+int tCG::p07(){ //   CALC -> E
 	return 0;}
-int tCG::p08(){ //    CALC -> BOOL
+int tCG::p08(){ //   CALC -> BOOL
 	return 0;}
-int tCG::p09(){ //    CALC -> STR
+int tCG::p09(){ //   CALC -> STR
 	return 0;}
-int tCG::p10(){ //    CALC -> DISPSET
+int tCG::p10(){ //   CALC -> DISPSET
 	return 0;}
-int tCG::p11(){ //       E -> $id
+int tCG::p11(){ //      E -> $id
 	return 0;}
-int tCG::p12(){ //       E -> CONST
+int tCG::p12(){ //      E -> $zero
 	return 0;}
-int tCG::p13(){ //       E -> ADD
+int tCG::p13(){ //      E -> ADD
 	return 0;}
-int tCG::p14(){ //       E -> SUB
+int tCG::p14(){ //      E -> SUB
 	return 0;}
-int tCG::p15(){ //       E -> DIV
+int tCG::p15(){ //      E -> DIV
 	return 0;}
-int tCG::p16(){ //       E -> MUL
+int tCG::p16(){ //      E -> MUL
 	return 0;}
-int tCG::p17(){ //       E -> COND
+int tCG::p17(){ //      E -> COND
 	return 0;}
-int tCG::p18(){ //       E -> IF
+int tCG::p18(){ //      E -> IF
 	return 0;}
-int tCG::p19(){ //       E -> CPROC
+int tCG::p19(){ //      E -> CPROC
 	return 0;}
-int tCG::p20(){ //   CONST -> $zero
+int tCG::p20(){ //    ADD -> HADD E )
 	return 0;}
-int tCG::p21(){ //   CONST -> $dec
+int tCG::p21(){ //   HADD -> ( +
 	return 0;}
-int tCG::p22(){ //     ADD -> HADD E )
+int tCG::p22(){ //   HADD -> HADD E
 	return 0;}
-int tCG::p23(){ //    HADD -> ( +
+int tCG::p23(){ //    SUB -> HSUB E )
 	return 0;}
-int tCG::p24(){ //    HADD -> HADD E
+int tCG::p24(){ //   HSUB -> ( -
 	return 0;}
-int tCG::p25(){ //     SUB -> HSUB E )
+int tCG::p25(){ //   HSUB -> HSUB E
 	return 0;}
-int tCG::p26(){ //    HSUB -> ( -
+int tCG::p26(){ //    DIV -> HDIV E )
 	return 0;}
-int tCG::p27(){ //    HSUB -> HSUB E
+int tCG::p27(){ //   HDIV -> ( /
 	return 0;}
-int tCG::p28(){ //     DIV -> HDIV E )
+int tCG::p28(){ //   HDIV -> HDIV E
 	return 0;}
-int tCG::p29(){ //    HDIV -> ( /
+int tCG::p29(){ //    MUL -> HMUL E )
 	return 0;}
-int tCG::p30(){ //    HDIV -> HDIV E
+int tCG::p30(){ //   HMUL -> ( *
 	return 0;}
-int tCG::p31(){ //     MUL -> HMUL E )
+int tCG::p31(){ //   HMUL -> HMUL E
 	return 0;}
-int tCG::p32(){ //    HMUL -> ( *
+int tCG::p32(){ //   COND -> HCOND ELSE )
 	return 0;}
-int tCG::p33(){ //    HMUL -> HMUL E
+int tCG::p33(){ //   COND -> HCOND CLAUS )
 	return 0;}
-int tCG::p34(){ //    COND -> HCOND ELSE )
+int tCG::p34(){ //  HCOND -> ( cond
 	return 0;}
-int tCG::p35(){ //    COND -> HCOND CLAUS )
+int tCG::p35(){ //  HCOND -> HCOND CLAUS
 	return 0;}
-int tCG::p36(){ //   HCOND -> ( cond
+int tCG::p36(){ //  CLAUS -> HCLAUS E )
 	return 0;}
-int tCG::p37(){ //   HCOND -> HCOND CLAUS
+int tCG::p37(){ // HCLAUS -> ( BOOL
 	return 0;}
-int tCG::p38(){ //   CLAUS -> HCLAUS E )
+int tCG::p38(){ // HCLAUS -> HCLAUS INTER
 	return 0;}
-int tCG::p39(){ //  HCLAUS -> ( BOOL
+int tCG::p39(){ //   ELSE -> HELSE E )
 	return 0;}
-int tCG::p40(){ //  HCLAUS -> HCLAUS INTER
+int tCG::p40(){ //  HELSE -> ( else
 	return 0;}
-int tCG::p41(){ //    ELSE -> HELSE E )
+int tCG::p41(){ //  HELSE -> HELSE INTER
 	return 0;}
-int tCG::p42(){ //   HELSE -> ( else
+int tCG::p42(){ //     IF -> ( if BOOL E E )
 	return 0;}
-int tCG::p43(){ //   HELSE -> HELSE INTER
+int tCG::p43(){ //  CPROC -> HCPROC )
 	return 0;}
-int tCG::p44(){ //      IF -> ( if BOOL E E )
+int tCG::p44(){ // HCPROC -> ( $id
 	return 0;}
-int tCG::p45(){ //   CPROC -> HCPROC )
+int tCG::p45(){ // HCPROC -> HCPROC E
 	return 0;}
-int tCG::p46(){ //  HCPROC -> ( $id
+int tCG::p46(){ //   BOOL -> $bool
 	return 0;}
-int tCG::p47(){ //  HCPROC -> HCPROC E
+int tCG::p47(){ //   BOOL -> $idq
 	return 0;}
-int tCG::p48(){ //    BOOL -> $bool
+int tCG::p48(){ //   BOOL -> CPRED
 	return 0;}
-int tCG::p49(){ //    BOOL -> $idq
+int tCG::p49(){ //   BOOL -> REL
 	return 0;}
-int tCG::p50(){ //    BOOL -> CPRED
+int tCG::p50(){ //   BOOL -> ( not BOOL )
 	return 0;}
-int tCG::p51(){ //    BOOL -> REL
+int tCG::p51(){ //  CPRED -> HCPRED )
 	return 0;}
-int tCG::p52(){ //    BOOL -> OR
+int tCG::p52(){ // HCPRED -> ( $idq
 	return 0;}
-int tCG::p53(){ //    BOOL -> AND
+int tCG::p53(){ // HCPRED -> HCPRED ARG
 	return 0;}
-int tCG::p54(){ //    BOOL -> ( not BOOL )
+int tCG::p54(){ //    ARG -> E
 	return 0;}
-int tCG::p55(){ //   CPRED -> HCPRED )
+int tCG::p55(){ //    ARG -> BOOL
 	return 0;}
-int tCG::p56(){ //  HCPRED -> ( $idq
+int tCG::p56(){ //    REL -> ( < E E )
 	return 0;}
-int tCG::p57(){ //  HCPRED -> HCPRED ARG
+int tCG::p57(){ //    REL -> ( = E E )
 	return 0;}
-int tCG::p58(){ //     ARG -> E
+int tCG::p58(){ //    STR -> $str
 	return 0;}
-int tCG::p59(){ //     ARG -> BOOL
+int tCG::p59(){ //    SET -> ( set! $id E )
 	return 0;}
-int tCG::p60(){ //     REL -> ( < E E )
+int tCG::p60(){ //DISPSET -> ( display E )
 	return 0;}
-int tCG::p61(){ //     REL -> ( = E E )
+int tCG::p61(){ //DISPSET -> ( display BOOL )
 	return 0;}
-int tCG::p62(){ //     REL -> ( > E E )
+int tCG::p62(){ //DISPSET -> ( display STR )
 	return 0;}
-int tCG::p63(){ //     REL -> ( >= E E )
+int tCG::p63(){ //DISPSET -> ( newline )
 	return 0;}
-int tCG::p64(){ //     REL -> ( <= E E )
+int tCG::p64(){ //DISPSET -> SET
 	return 0;}
-int tCG::p65(){ //      OR -> HOR BOOL )
+int tCG::p65(){ //  INTER -> DISPSET
 	return 0;}
-int tCG::p66(){ //     HOR -> ( or
+int tCG::p66(){ //  INTER -> E
 	return 0;}
-int tCG::p67(){ //     HOR -> HOR BOOL
+int tCG::p67(){ //   DEFS -> DEF
 	return 0;}
-int tCG::p68(){ //     AND -> HAND BOOL )
+int tCG::p68(){ //   DEFS -> DEFS DEF
 	return 0;}
-int tCG::p69(){ //    HAND -> ( and
+int tCG::p69(){ //    DEF -> PRED
 	return 0;}
-int tCG::p70(){ //    HAND -> HAND BOOL
+int tCG::p70(){ //    DEF -> VAR
 	return 0;}
-int tCG::p71(){ //     STR -> $str
+int tCG::p71(){ //    DEF -> PROC
 	return 0;}
-int tCG::p72(){ //     STR -> SIF
+int tCG::p72(){ //   PRED -> HPRED BOOL )
 	return 0;}
-int tCG::p73(){ //     SIF -> ( if BOOL STR STR )
+int tCG::p73(){ //  HPRED -> PDPAR )
 	return 0;}
-int tCG::p74(){ //     SET -> ( set! $id E )
+int tCG::p74(){ //  PDPAR -> ( define ( $idq
 	return 0;}
-int tCG::p75(){ // DISPSET -> ( display E )
+int tCG::p75(){ //  PDPAR -> PDPAR $idq
 	return 0;}
-int tCG::p76(){ // DISPSET -> ( display BOOL )
+int tCG::p76(){ //  PDPAR -> PDPAR $id
 	return 0;}
-int tCG::p77(){ // DISPSET -> ( display STR )
+int tCG::p77(){ //  CONST -> $zero
 	return 0;}
-int tCG::p78(){ // DISPSET -> ( newline )
+int tCG::p78(){ //  CONST -> $dec
 	return 0;}
-int tCG::p79(){ // DISPSET -> SET
+int tCG::p79(){ //    VAR -> ( define $id CONST )
 	return 0;}
-int tCG::p80(){ //   INTER -> DISPSET
+int tCG::p80(){ //   PROC -> HPROC LET )
 	return 0;}
-int tCG::p81(){ //   INTER -> E
+int tCG::p81(){ //   PROC -> HPROC E )
 	return 0;}
-int tCG::p82(){ //    DEFS -> DEF
+int tCG::p82(){ //  HPROC -> PCPAR )
 	return 0;}
-int tCG::p83(){ //    DEFS -> DEFS DEF
+int tCG::p83(){ //  HPROC -> HPROC INTER
 	return 0;}
-int tCG::p84(){ //     DEF -> PRED
+int tCG::p84(){ //  PCPAR -> ( define ( $id
 	return 0;}
-int tCG::p85(){ //     DEF -> VAR
+int tCG::p85(){ //  PCPAR -> PCPAR $id
 	return 0;}
-int tCG::p86(){ //     DEF -> PROC
+int tCG::p86(){ //    LET -> HLET E )
 	return 0;}
-int tCG::p87(){ //    PRED -> HPRED BOOL )
+int tCG::p87(){ //   HLET -> LETLOC )
 	return 0;}
-int tCG::p88(){ //   HPRED -> PDPAR )
+int tCG::p88(){ //   HLET -> HLET INTER
 	return 0;}
-int tCG::p89(){ //   PDPAR -> ( define ( $idq
+int tCG::p89(){ // LETLOC -> ( let (
 	return 0;}
-int tCG::p90(){ //   PDPAR -> PDPAR $idq
+int tCG::p90(){ // LETLOC -> LETLOC LETVAR
 	return 0;}
-int tCG::p91(){ //   PDPAR -> PDPAR $id
-	return 0;}
-int tCG::p92(){ //     VAR -> ( define $id CONST )
-	return 0;}
-int tCG::p93(){ //    PROC -> HPROC LET )
-	return 0;}
-int tCG::p94(){ //    PROC -> HPROC E )
-	return 0;}
-int tCG::p95(){ //   HPROC -> PCPAR )
-	return 0;}
-int tCG::p96(){ //   HPROC -> HPROC INTER
-	return 0;}
-int tCG::p97(){ //   PCPAR -> ( define ( $id
-	return 0;}
-int tCG::p98(){ //   PCPAR -> PCPAR $id
-	return 0;}
-int tCG::p99(){ //     LET -> HLET E )
-	return 0;}
-int tCG::p100(){ //    HLET -> LETLOC )
-	return 0;}
-int tCG::p101(){ //    HLET -> HLET INTER
-	return 0;}
-int tCG::p102(){ //  LETLOC -> ( let (
-	return 0;}
-int tCG::p103(){ //  LETLOC -> LETLOC LETVAR
-	return 0;}
-int tCG::p104(){ //  LETVAR -> ( $id E )
+int tCG::p91(){ // LETVAR -> ( $id E )
 	return 0;}
 //_____________________
-int tCG::p105(){return 0;} int tCG::p106(){return 0;} 
-int tCG::p107(){return 0;} int tCG::p108(){return 0;} 
-int tCG::p109(){return 0;} int tCG::p110(){return 0;} 
-
+int tCG::p92(){return 0;} int tCG::p93(){return 0;} 
+int tCG::p94(){return 0;} int tCG::p95(){return 0;} 
+int tCG::p96(){return 0;} int tCG::p97(){return 0;} 
+int tCG::p98(){return 0;} int tCG::p99(){return 0;} 
+int tCG::p100(){return 0;} int tCG::p101(){return 0;} 
+int tCG::p102(){return 0;} int tCG::p103(){return 0;} 
+int tCG::p104(){return 0;} int tCG::p105(){return 0;} 
+int tCG::p106(){return 0;} int tCG::p107(){return 0;} 
+int tCG::p108(){return 0;} int tCG::p109(){return 0;} 
+int tCG::p110(){return 0;} 
 
